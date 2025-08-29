fb4e2094a4b3c39a68b3eb0204bfdefa67a57f87
ottsupi <ott.regisorate@gmail.com>
2025-08-29 07:01:39 +0000

    feat: add function to create person in the database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,14 @@
 const { createServer } = require("node:http");
+const { pool } = require("./db_conn");
+
+async function CreatePerson(lastName, firstName, age) {
+  const res = await pool.query(
+      "INSERT INTO persons (lastName, firstName, age) VALUES ($1, $2, $3)",
+      [lastName, firstName, age]
+    );
+}
+
+// CreatePerson("ottsupi", "dev", 24)
 
 const hostname = "0.0.0.0";
 const port = 3000;
