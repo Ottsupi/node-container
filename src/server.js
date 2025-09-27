6cf69965beea7d6bef0e80078324bb301464d638
ottsupi <ott.regisorate@gmail.com>
2025-09-27 14:55:28 +0800

    refactor: remove database

--- src/server.js
+++ src/server.js
@@ -1,17 +1,7 @@
 const { createServer } = require("node:http");
-const { pool } = require("./db_conn");
-
-async function CreatePerson(lastName, firstName, age) {
-  const res = await pool.query(
-      "INSERT INTO persons (lastName, firstName, age) VALUES ($1, $2, $3)",
-      [lastName, firstName, age]
-    );
-}
-
-// CreatePerson("ottsupi", "dev", 24)
 
 const hostname = "0.0.0.0";
-const port = 3000;
+const port = 5000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
